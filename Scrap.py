a30d55996bfca2af3721c40b9d800f1fe52fdc33
Mateo <manosalvas.mateof@gmail.com>
2024-08-01 10:27:05 -0500

    Create the view of both tables to use as a Data Warehouse

--- Scrap.py
+++ Scrap.py
@@ -40,6 +40,22 @@ def exchange_scraper():
                 variance_percentage real
             )""")
         print("Successful connection")
+        try:
+            cursor.execute("""CREATE OR REPLACE VIEW DataWarehouse
+                         AS
+                         SELECT t1.date,
+                            t2.time,
+                            t2.value AS exchange_value,
+                            t2.variance AS exchange_variance,
+                            t2.variance_percentage AS exchange_variance_percentage,
+                            t1.last_value AS risk_value,
+                            t1.variance AS risk_value_variance
+                           FROM arg_risk t1
+                             JOIN arg_exchange t2 ON t1.date = t2.date;
+                        """)
+            conn.commit()
+        except Error as e:
+            print(e)
         while True:
             url1 = 'https://es.investing.com/currencies/usd-ars'
             page1 = requests.get(url1)
@@ -91,6 +107,22 @@ def risk_scraper():
                 "variance" real
             )""")
         print("Successful connection")
+        try:
+            cursor.execute("""CREATE OR REPLACE VIEW DataWarehouse
+                         AS
+                         SELECT t1.date,
+                            t2.time,
+                            t2.value AS exchange_value,
+                            t2.variance AS exchange_variance,
+                            t2.variance_percentage AS exchange_variance_percentage,
+                            t1.last_value AS risk_value,
+                            t1.variance AS risk_value_variance
+                           FROM arg_risk t1
+                             JOIN arg_exchange t2 ON t1.date = t2.date;
+                        """)
+            conn.commit()
+        except Error as e:
+            print(e)
         while True:
             driver = webdriver.Edge()
             driver.get('https://www.ambito.com/contenidos/riesgo-pais.html')
@@ -111,13 +143,30 @@ def risk_scraper():
             for i in crv:
                 risk.append(i.text)
                 variance = float(i.text[1:4].replace(',', '.'))
-            print(risk[-3] + " | " + risk[-2] + " | " + risk[-1])
+            try:
+                print(risk[-3] + " | " + risk[-2] + " | " + risk[-1])
+            except Error as e:
+                print(e)
+                # Country risk
+                rsk = soup2.find_all('span', class_='variation-last__value value data-ultimo')
+                # print(rsk)
+                # Country risk
+                crv = soup2.find_all('span',
+                                     class_='variation-last__percent percent data-class-variacion data-variacion indicators__value equal')
+                # print(crv)
+                for i in rsk:
+                    risk.append(i.text)
+                    value = float(i.text.replace(',', '.'))
+                for i in crv:
+                    risk.append(i.text)
+                    variance = float(i.text[1:4].replace(',', '.'))
+                print(risk[-3] + " | " + risk[-2] + " | " + risk[-1])
             cursor.execute("""INSERT INTO arg_risk
                             (date, last_value, variance)
                             VALUES (%s, %s, %s)""", (date, value, variance))
             cursor.execute("SELECT * FROM arg_risk")
             conn.commit()
-            time.sleep(28800)
+            time.sleep(86400)
     except Error as e:
         print(e)
 
